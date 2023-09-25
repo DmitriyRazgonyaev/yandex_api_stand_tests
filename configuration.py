831c5157cb7c54ad912b6cdf3bb775bd4dc1e30b
unknown <kozyavkagreat@gmail.com>
2023-09-25 14:25:38 +0500

    My 3 commit

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://68ef8028-34ca-491a-8e9e-fcfc85333075.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://a548459f-46d4-4353-a43c-efb11dfd3ea7.serverhub.praktikum-services.ru"
 CREATE_USER_PATH = "/api/v1/users/"
 Main_Kits = "/api/v1/kits"
